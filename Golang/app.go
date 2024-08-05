79ba9b30224157cd2c8e63372773d39409a4b2d8
lokesh220 <lokeshchoraria2@gmail.com.com>
2024-08-06 03:48:53 +0530

    complted git proj

--- Golang/app.go
+++ Golang/app.go
@@ -295,6 +295,102 @@ func buildCols(keys []int,commits map[int]int)map[int]column{
 	}
 	return cols
 }
+
+//
+func printCells(cols map[int]column){
+	printMonths()
+	for j:=6;j>=0;j--{
+		for i:=weeksInLastSixMonths +1 ;i>=0 ;i--{
+			if i==weeksInLastSixMonths+1{
+				printDayCol(j)
+			}
+			if col,ok:=cols[i];ok{
+				if i==0&&j==calcOffset()-1{
+					printCell(col[j],true)
+					continue
+				}else{
+					if len(col)>j{
+						printCell(col[j],false)
+						continue
+					}
+				}
+			}
+			printCell(0,false)
+		}
+		fmt.Printf("\n")
+	}
+}
+
+//
+func  printMonths(){
+	week:=getBeginningOfDay(time.Now()).Add(-(daysInLastSixMonths*time.Hour*24))
+	month:=week.Month()
+	fmt.Printf("        ")
+	for{
+		if week.Month()!=month{
+			fmt.Printf("%s",week.Month().String()[:3])
+			month=week.Month()
+		}else{
+			fmt.Printf("     ")
+		}
+		week=week.Add(7*time.Hour*24)
+		if week.After(time.Now()){
+			break
+		}
+	}
+	fmt.Printf("\n")
+}
+
+
+//
+func printDayCol(day int){
+	out:="    "
+	switch day{
+	case 1:
+		out="MON"
+		case 3:
+		out="WED"
+		case 5:
+		out="FRI"
+	}
+	fmt.Printf(out)
+}
+
+//
+func printCell(val int,today bool){
+	escape :="\033[0;37;30m"
+	switch {
+    case val > 0 && val < 5:
+        escape = "\033[1;30;47m"
+    case val >= 5 && val < 10:
+        escape = "\033[1;30;43m"
+    case val >= 10:
+        escape = "\033[1;30;42m"
+    }
+
+    if today {
+        escape = "\033[1;37;45m"
+    }
+
+    if val == 0 {
+        fmt.Printf(escape + "  - " + "\033[0m")
+        return
+    }
+
+    str := "  %d "
+    switch {
+    case val >= 10:
+        str = " %d "
+    case val >= 100:
+        str = "%d "
+    }
+
+    fmt.Printf(escape+str+"\033[0m", val)
+}
+
+
+//
+
 func main() {
 
 	var folder string
